77a621c311e63d3e30d43a35aeed3c470ce3e8c1
dingyaozhang <59897897+dingyaozhang@users.noreply.github.com>
2022-04-09 23:31:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'SW_Algorithm',
+    name = 'SWAlgorithm',
     version = '0.1.4',
     keywords='SW',
     license = 'MIT License',
